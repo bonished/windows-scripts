66b1b927251212e096f205a0c3191464f36422ce
bonished <47390256+bonished@users.noreply.github.com>
2022-12-03 16:59:12 +0100

    Update SerialKeyExtraction.ps1
    Serial key is now also copied to clipboard

--- SerialKeyExtraction.ps1
+++ SerialKeyExtraction.ps1
@@ -1,4 +1,4 @@
 $key = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform" -Name BackupProductKeyDefault).BackupProductKeyDefault
-Write-Host "Windows serial key is:" -NoNewline $key
-Write-Host
+Write-Host "Windows serial key is" $key "and it has been copied to clipboard." 
+Set-Clipboard $key
 Read-Host "Press enter to continue..."

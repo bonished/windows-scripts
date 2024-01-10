dd77056828b18490b0b32d60f4b561ab6246459e
bonished <47390256+bonished@users.noreply.github.com>
2024-01-10 09:09:37 +0100

    Update WindowsPatchChecker.ps1
    Rewritten progress bar to show server number from server list, name of currently processed server and how many more left to check.
    Changed and moved readiness message so now it will always show whether KBs are provided from file or automatically.
    Added console clear on check start.

--- WindowsPatchChecker.ps1
+++ WindowsPatchChecker.ps1
@@ -1,18 +1,15 @@
 [CmdletBinding()]
 param ()
-# Tested only on 2016 and 2019
+
 # Title
 Write-Output "===== KB Check for Windows Server 2012, 2016 and 2019 ====="
 
 # Read the server names from a text file
 $TextFile = Get-Content -Path "$PSScriptRoot\servers.txt"
 
-# Get the total number of objects in the text file
-$totalObjects = $TextFile.Count
-
 # Define the KBs to check for different operating systems
 $W2k19KBs = @("KB5026362")
-$W2k16KBs = @("KB5026363")
+$W2k16KBs = @("KB5033373")
 $W2k12KBs = @("KB5018474")
 
 # Check if Verbose mode is enabled
@@ -22,11 +19,14 @@ $VerboseMode = $PSBoundParameters['Verbose']
 $iteration = 0
 $failedServersCount = 0
 
+# Check how many servers are listed in server file
+$lineCount = Get-Content -Path "$PSScriptRoot\servers.txt" | Measure-Object -Line | Select-Object -ExpandProperty Lines
+
 # Add the required assembly for using the MessageBox class
 Add-Type -AssemblyName System.Windows.Forms
 
 # Display a warning message
-Write-Warning "Please ensure that you have manually entered the correct KB numbers in the script before proceeding or set it after this prompt."
+Write-Warning "WARNING: Please ensure that you have manually entered the correct KB numbers in the script before proceeding or set it after this prompt."
 
 # Prompt the user to confirm if they have entered the KB numbers
 $confirmInput = ''
@@ -45,94 +45,100 @@ if ($confirmInput -in @('n', 'no')) {
         $W2k19KBs = Read-Host "Enter the KB number for Windows Server 2019:"
         $W2k16KBs = Read-Host "Enter the KB number for Windows Server 2016:"
         $W2k12KBs = Read-Host "Enter the KB number for Windows Server 2012:"
-        Write-Output "KB numbers set, starting check."
+        
     } else {
         Write-Host "Script execution cancelled. Please manually enter the KB numbers and run the script again."
         [System.Windows.Forms.MessageBox]::Show("Script execution cancelled. Please manually enter the KB numbers and run the script again or set them while running script.", "Script Execution Cancelled", "OK", "Warning")
         exit
     }
 }
+Clear-Host
+Write-Output "===== KB Check for Windows Server 2012, 2016 and 2019 ====="
+Write-Output "KB numbers set, starting check. There are $lineCount servers to process."
 
 # Initialize an array to store the output objects
 $output = foreach ($ServerName in $TextFile) {
     $iteration++
 
+    # How many servers left
+    $serversLeft = $lineCount - $iteration
+    
+    # Display the progress bar
+    Write-Progress -Activity "Processing Servers" -Status "Processing Server $ServerName $iteration/$lineCount, $serversLeft more servers left to complete." -PercentComplete (($iteration / $lineCount) * 100)
+
+
     # Create a PowerShell remoting session to the target server
     $sessionOSVersion = New-PSSession -ComputerName $ServerName -ErrorAction SilentlyContinue
 
-    # Invoke a command on the remote session to retrieve information
-    $result = Invoke-Command -Session $sessionOSVersion -ScriptBlock {
-        # Get the computer system information
-        $computerSystem = Get-CimInstance -ClassName Win32_ComputerSystem
+    try {
+        # Invoke a command on the remote session to retrieve information
+        $result = Invoke-Command -Session $sessionOSVersion -ScriptBlock {
+            # Get the computer system information
+            $computerSystem = Get-CimInstance -ClassName Win32_ComputerSystem
 
-        # Get the operating system information
-        $operatingSystem = Get-WmiObject -Class Win32_OperatingSystem
+            # Get the operating system information
+            $operatingSystem = Get-WmiObject -Class Win32_OperatingSystem
 
-        # Initialize variable to store the installed hotfixes
-        $hotfixes = "No KBs to check"
+            # Initialize variable to store the installed hotfixes
+            $hotfixes = "No KBs to check"
 
-        # Check the operating system version and retrieve corresponding hotfixes
-        if ($operatingSystem.Name -like "*2019*") {
-            $hotfixes = Get-HotFix -Id $using:W2k19KBs -ErrorAction SilentlyContinue
+            # Check the operating system version and retrieve corresponding hotfixes
+            if ($operatingSystem.Name -like "*2019*") {
+                $hotfixes = Get-HotFix -Id $using:W2k19KBs -ErrorAction SilentlyContinue
+            }
+            elseif ($operatingSystem.Name -like "*2016*") {
+                $hotfixes = Get-HotFix -Id $using:W2k16KBs -ErrorAction SilentlyContinue
+            }
+            elseif ($operatingSystem.Name -like "*2012*") {
+                $hotfixes = Get-HotFix -Id $using:W2k12KBs -ErrorAction SilentlyContinue
+            }
+
+            # Create a custom object to represent the server and hotfix information
+            [PSCustomObject]@{
+                Hostname = $computerSystem.Name
+                InstalledHotfix = $hotfixes.HotFixID
+                InstalledOn = $hotfixes.InstalledOn
+                Failed = $null
+            }
         }
-        elseif ($operatingSystem.Name -like "*2016*") {
-            $hotfixes = Get-HotFix -Id $using:W2k16KBs -ErrorAction SilentlyContinue
+
+        # Check if the hotfix was not found on the server
+        if (-not $result.InstalledHotfix) {
+            $result.InstalledHotfix = "Patch missing"
+            $result.InstalledOn = "--/--/---- --:--:--"
+            $result.Failed = $true
+            $failedServersCount++  # Increment the count of failed servers
+        } else {
+            $result.Failed = $false # Mark the server as succeeded
         }
-        elseif ($operatingSystem.Name -like "*2012*") {
-            $hotfixes = Get-HotFix -Id $using:W2k12KBs -ErrorAction SilentlyContinue
+        # Create an output object with selected properties
+        $outputObject = $result | Select-Object Hostname, InstalledHotfix, InstalledOn, Failed
+
+        # Store the output object in an array
+        $outputObject
+
+        # Display the information on the console
+        Write-Host "Hostname:" -NoNewline
+        Write-Host " $($result.Hostname)" -ForegroundColor Yellow -NoNewline
+        Write-Host " Installed Hotfix:" -NoNewline
+        if ($result.Failed) {
+            Write-Host " $($result.InstalledHotfix)" -ForegroundColor Red -NoNewline
+        } else {
+            Write-Host " $($result.InstalledHotfix)" -ForegroundColor Green -NoNewline
         }
-
-        # Create a custom object to represent the server and hotfix information
-        [PSCustomObject]@{
-            Hostname = $computerSystem.Name
-            InstalledHotfix = $hotfixes.HotFixID
-            InstalledOn = $hotfixes.InstalledOn
-            Failed = $null
+        Write-Host " Installed on:" -NoNewline
+        if ($result.Failed) {
+            Write-Host " $($result.InstalledOn)" -ForegroundColor Red
+        } else {
+            Write-Host " $($result.InstalledOn)" -ForegroundColor Green
         }
+    } catch {
+        Write-Host "Error occurred while processing server: $ServerName"
+    } finally {
+        # Clean up the PowerShell remoting session
+        Start-Sleep 0
+        $sessionOSVersion | Remove-PSSession
     }
-
-    # Check if the hotfix was not found on the server
-    if (-not $result.InstalledHotfix) {
-        $result.InstalledHotfix = "Patch missing"
-        $result.InstalledOn = "--/--/---- --:--:--"
-        $result.Failed = $true
-        $failedServersCount++  # Increment the count of failed servers
-    } else {
-        $result.Failed = $false # Mark the server as succeeded
-    }
-    # Create an output object with selected properties
-    $outputObject = $result | Select-Object Hostname, InstalledHotfix, InstalledOn, Failed
-
-    # Store the output object in an array
-    $outputObject
-
-    # Display the information on the console
-    Write-Host "Hostname:" -NoNewline
-    if ($result.Failed) {
-        Write-Host " $($result.Hostname)" -ForegroundColor Red -NoNewline
-    } else {
-        Write-Host " $($result.Hostname)" -ForegroundColor Green -NoNewline
-    }    
-    Write-Host " Installed Hotfix:" -NoNewline
-    if ($result.Failed) {
-        Write-Host " $($result.InstalledHotfix)" -ForegroundColor Red -NoNewline
-    } else {
-        Write-Host " $($result.InstalledHotfix)" -ForegroundColor Green -NoNewline
-    }
-    Write-Host " Installed on:" -NoNewline
-    if ($result.Failed) {
-        Write-Host " $($result.InstalledOn)" -ForegroundColor Red
-    } else {
-        Write-Host " $($result.InstalledOn)" -ForegroundColor Green
-    }
-
-    # Update the progress bar
-    $progressText = "Processing $iteration out of $totalObjects servers"
-    Write-Progress -Activity "Checking KBs" -Status $progressText -PercentComplete (($iteration/$totalObjects) * 100)
-
-    # Clean up the PowerShell remoting session
-    Start-Sleep 0
-    $sessionOSVersion | Remove-PSSession
 
     # Display verbose message if Verbose mode is enabled
     if ($VerboseMode) {
@@ -140,8 +146,11 @@ $output = foreach ($ServerName in $TextFile) {
     }
 }
 
+# Complete the progress bar
+Write-Progress -Activity "Processing Servers" -Status "Processing Complete" -Completed
+
 # Count the number of iterations and display the total number of servers
-Write-Host "Total servers listed: $iteration"
+Write-Host "Total servers processed: $iteration"
 if ($VerboseMode) {
     Write-Verbose "Completed counting servers processed."
 }
@@ -163,22 +172,6 @@ while ($generateReport -notin @('y', 'n', 'yes', 'no')) {
 if ($generateReport -in @('y', 'yes')) {
     # Open the output in Out-GridView for easy viewing
     $output | Out-GridView
-}
-
-# Prompt the user if they want to save the output in a text file
-$saveToFile = ''
-while ($saveToFile -notin @('y', 'n', 'yes', 'no')) {
-    $saveToFile = Read-Host "Do you want to save the output in a text file? (Yes/No)"
-
-    if ($saveToFile -notin @('y', 'n', 'yes', 'no')) {
-        Write-Host "Invalid input. Please enter 'y' or 'n'."
-    }
-}
-
-if ($saveToFile -in @('y', 'yes')) {
-    $fileName = Join-Path -Path $PSScriptRoot -ChildPath "KB_Check_Report.txt"
-    $output | Export-Csv -Path $fileName -NoTypeInformation
-    Write-Host "Output saved to: $fileName"
 }
 
 # Prompt the user to press a key before exiting
